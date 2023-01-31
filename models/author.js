c52912e118e58c6ab437e1a7dae10233d4caf2bb
giovanniPepi <gtpepi@proton.me>
2023-01-31 16:36:30 -0300

    Update db schemas

--- models/author.js
+++ models/author.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  first_name: { type: String, required: true, maxLenght: 100 },
-  family_name: { type: String, required: true, maxLenght: 100 },
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
